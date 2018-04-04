48a14fa4be5875d343bcaaee3198d587d976d402
zer@kth.se <zer@kth.se>
2018-04-04 11:49:55 +0200

    Final commit. Commiting complete solution.

--- src/main/scala/simpret/interpreter/Interpreter.scala
+++ src/main/scala/simpret/interpreter/Interpreter.scala
@@ -8,7 +8,6 @@ final case class EvaluationException(private val message: String, private val x:
                                      private val cause: Throwable = None.orNull)
   extends Exception(message, cause)
 
-
 object Interpreter {
   def errFun(msg: String, x: AST) = throw new EvaluationException(msg, x)
 
@@ -21,7 +20,52 @@ object Interpreter {
 
   /* evaluation function for taking one step at a time */
   def step(x: AST, store: Map[String, AST]): Option[(AST, Map[String, AST])] = {
-    None
+    x match {
+      case Variable(id) => {
+        if(store.contains(id)) {
+          Option((store(id), store))
+        } else {
+          errFun("Variable not found", x)
+        }
+      }
+      case CondExp(BoolLit(b), e1, e2) if b => Option(e1, store)
+      case CondExp(BoolLit(b), e1, e2) if !b => Option(e2, store)
+      case CondExp(IntLit(i), e1, e2) => errFun("condition should be a boolean expression", x)
+      case CondExp(c, e1, e2) => {
+        val (cp, storep) = step(c, store).get
+        Option((CondExp(cp, e1, e2), storep))
+      }
+      case IsZeroExp(IntLit(i)) if i == 0 => Option(BoolLit(true), store)
+      case IsZeroExp(IntLit(i)) if i != 0 => Option(BoolLit(false), store)
+      case IsZeroExp(BoolLit(b)) => errFun("iszero expects integer value", x)
+      case IsZeroExp(e) => {
+        val (ep, storep) = step(e, store).get
+        Option((IsZeroExp(ep), storep))
+      }
+      case PlusExp(BoolLit(b), _) => errFun("addition operator should be integer expression", x)
+      case PlusExp(_, BoolLit(b)) => errFun("addition operator should be integer expression", x)
+      case PlusExp(IntLit(i1), IntLit(i2)) => Option(IntLit(i1 + i2), store)
+      case PlusExp(IntLit(i1), e) => {
+        val (ep, storep) = step(e, store).get
+        Option((PlusExp(IntLit(i1), ep), storep))
+      }
+      case PlusExp(e1, e2) => {
+        val (e1p, storep) = step(e1, store).get
+        Option((PlusExp(e1p, e2), storep))
+      }
+      case AssignExp(id, e) if isvalue(e) => Option(e, store + (id -> e))
+      case AssignExp(id, e) => {
+        val (ep, storep) = step(e, store).get
+        Option((AssignExp(id, ep), storep))
+      }
+      case SeqExp(e1, e2) if isvalue(e1) => Option(e2, store)
+      case SeqExp(e1, e2) => {
+        val (e1p, storep) = step(e1, store).get
+        Option((SeqExp(e1p, e2), storep))
+      }
+      case _ if isvalue(x) => None
+      case _ => errFun("Invalid syntax tree", x)
+    }
   }
 
   /* evaluation function to iterate the steps of evaluation */
